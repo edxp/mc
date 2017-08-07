02ab4c50a84c0b10b0e525955a1615eac09f2757
Nick <dota1803@gmail.com>
2017-08-07 11:54:31 +0300

    RC/emmonitor: Bug fixes: 	- Revealed weird internal comparator behaviour on chunk loading/unloading 	- Added timer with animated [|] shit to monitor system aliveness 	- Added additional battery check: if power below 10%, then give pendal' out 	- Added simple logging with date/time at /var/log/emmonitor.log 	- Fixed not existing UI constant

--- OpenComputers/ReactorController/emmonitor.lua
+++ OpenComputers/ReactorController/emmonitor.lua
@@ -1,20 +1,22 @@
 local component = require("component")
-local robot = require("robot")
 local computer = require("computer")
+local robot = require("robot")
+--local event = require("event")
 local term = require("term")
 local sides = require("sides")
 local note = require("note")
 
 -- Settings
 --=======================================================================
-local requiredRedstoneLevel = 14
+local requiredRedstoneLevel = 8
 local redstoneComparatorSide = sides.back
-local redstoneAlertSide = sides.right
+local redstoneAlertSide = sides.left
 local useInternalComparator = false
 --=======================================================================
 
 local redstone
-local isRunning = false
+local animatedString = "|/-\\|/-\\"
+local animatedStringIndex = 1
 
 -- #define... :)
 local REDSTONE_FALSE = 0
@@ -32,21 +34,42 @@ local UI_CTRL_ELECTROMAGNETS = 203
 local UI_CTRL_ALERT = 204
 local UI_CTRL_CURRENTSTATUS = 205
 local UI_CTRL_REDSTONELEVEL = 206
+local UI_CTRL_BOTTOMBAR = 207
 
-local STR_GOOD =                        "GOOD  "
-local STR_BAD =                         "BAD   "
-local STR_READY =                       "READY "
-local STR_FAIL =                        "FAIL  "
-local STR_CURRENT_REDSTONE_LEVEL =      "Current redstone level                            "
-local STR_CONDITIONS_NOT_MET =          "ONE OR MORE CONDITIONS ARE NOT MET                "
-local STR_UNMOUNTING_CORE =             "UNMOUNTING CORE...                                "
-local STR_UNMOUNT_SUCCESS =             "REACTOR CORE HAS BEEN SUCCESSFULLY UNMOUNTED      "
-local STR_UNMOUNT_FAIL =                "FAILED TO UNMOUNT REACTOR CORE                    "
-local STR_CHECK_ASAP =                  "CHECK CURRENT CONFIGURATION ASAP                  "
-local STR_NOTHING =                     "                                                  "
+local STR_GOOD =                        "GOOD   "
+local STR_BAD =                         "BAD    "
+local STR_READY =                       "GOOD   "
+local STR_FAIL =                        "WARNING"
+local STR_ALERT =                       "ALERT  "
+local STR_CURRENT_REDSTONE_LEVEL =      "Current redstone level                "
+local STR_CONDITIONS_NOT_MET =          "ONE OR MORE CONDITIONS ARE NOT MET    "
+local STR_UNMOUNTING_CORE =             "UNMOUNTING CORE...                    "
+local STR_UNMOUNT_SUCCESS =             "CORE HAS BEEN SUCCESSFULLY UNMOUNTED  "
+local STR_UNMOUNT_FAIL =                "FAILED TO UNMOUNT REACTOR CORE        "
+local STR_CHECK_ASAP =                  "CHECK CURRENT CONFIGURATION ASAP      "
+local STR_NOTHING =                     "                                      "
+
+local function ClearLog()
+    local logFile = io.open("/var/log/emmonitor.log", "w")
+    logFile:close()
+
+    return true
+end
+
+local function LogLine(str)
+    local logFile = io.open("/var/log/emmonitor.log", "a")
+    logFile:write("["..os.date().."] "..str)
+    logFile:close()
+
+    return true
+end
+
+local function GetInternalPower()
+    return (computer.energy() / computer.maxEnergy())
+end
 
 local function CheckInternalPower()
-    return ((computer.energy() / computer.maxEnergy()) >= 0.5)
+    return (GetInternalPower() >= 0.5)
 end
 
 local function CheckToolValidity()
@@ -64,6 +87,7 @@ local function CheckInventorySpace()
     return false
 end
 
+-- Use normal or comparator signals
 local function GetCurrentRedstoneLevel()
     if (useInternalComparator) then
         return redstone.getComparatorInput(redstoneComparatorSide)
@@ -73,9 +97,13 @@ local function GetCurrentRedstoneLevel()
 end
 
 local function CheckElectromagnetsPower()
-    return (GetCurrentRedstoneLevel() >= requiredRedstoneLevel)
+    local currentRedstoneLevel = GetCurrentRedstoneLevel()
+    LogLine("Current redstone level: "..currentRedstoneLevel.."\n")
+
+    return (currentRedstoneLevel >= requiredRedstoneLevel)
 end
 
+-- Use normal only signal
 local function CheckAlertStatus()
     return ((redstone.getInput(redstoneAlertSide)) == REDSTONE_FALSE)
 end
@@ -83,12 +111,10 @@ end
 local function ReactorControl(state)
     if (state == REACTOR_ON) then
         redstone.setOutput(sides.front, REDSTONE_FALSE)
-        isRunning = true
 
         return true
     elseif (state == REACTOR_OFF) then
         redstone.setOutput(sides.front, REDSTONE_TRUE)
-        isRunning = false
 
         return true
     end
@@ -101,12 +127,15 @@ local function SetAlertStatus(state)
         redstone.setOutput(redstoneAlertSide, REDSTONE_TRUE)
         os.sleep(0.2)
         redstone.setOutput(redstoneAlertSide, REDSTONE_FALSE)
-    else
-        -- Reserved for future
+
+        return true
+    elseif (state == STATUS_ALERT_LOW) then
         redstone.setOutput(redstoneAlertSide, REDSTONE_FALSE)
+
+        return true
     end
 
-    return true
+    return false
 end
 
 local function CoreUnmount()
@@ -117,33 +146,55 @@ local function CoreUnmount()
         swingResult = true
     end
 
-    -- suck anyway, just in case
+    -- suck dick anyway, just in case
     robot.suck()
 
     return swingResult
 end
 
+local function ResetRedstoneOutput()
+    for robotSide = 0, 5, 1 do
+        redstone.setOutput(robotSide, REDSTONE_FALSE)
+    end
+end
+
 local function UpdateScreenStatus(field, status)
     if (field == UI_CTRL_POWER) then
-        term.setCursor(45, 8)
+        term.setCursor(44, 8)
     elseif (field == UI_CTRL_TOOL) then
-        term.setCursor(45, 9)
+        term.setCursor(44, 9)
     elseif (field == UI_CTRL_SPACE) then
-        term.setCursor(45, 10)
+        term.setCursor(44, 10)
     elseif (field == UI_CTRL_ELECTROMAGNETS) then
-        term.setCursor(45, 11)
+        term.setCursor(44, 11)
     elseif (field == UI_CTRL_ALERT) then
-        term.setCursor(45, 12)
+        term.setCursor(44, 12)
     elseif (field == UI_CTRL_CURRENTSTATUS) then
-        term.setCursor(45, 14)
+        term.setCursor(44, 14)
     elseif (field == UI_CTRL_REDSTONELEVEL) then
-        term.setCursor(45, 15)
+        term.setCursor(44, 15)
     elseif (field == UI_CTRL_BOTTOMBAR) then
         term.setCursor(1, 16)
+    elseif (field == UI_CTRL_TIME) then
+        term.setCursor(39, 16)
     end
 
     term.write(status, false)
     term.setCursor(1, 16)
+
+    return true
+end
+
+local function UpdateScreenTime()
+    local onlyTime = os.date()
+    onlyTime = onlyTime:sub(#onlyTime - 7, -1)
+    
+    UpdateScreenStatus(UI_CTRL_TIME, "["..animatedString:sub(animatedStringIndex, animatedStringIndex).."] "..onlyTime)
+    animatedStringIndex = animatedStringIndex + 1
+
+    if (animatedStringIndex > 8) then
+        animatedStringIndex = 1
+    end
 
     return true
 end
@@ -156,6 +207,9 @@ local function main()
 
     local isCompatible = true
 
+    ClearLog()
+    LogLine("Log file opened\n")
+
     if (not component.isAvailable("robot")) then
         io.stderr:write("This program requires robot to run\n")
         isCompatible = false
@@ -166,20 +220,20 @@ local function main()
 
     if (not isCompatible) then
         io.write("==================================================\n")
+        LogLine("System is NOT compatible\n")
         os.exit()
     end
+
+    LogLine("System is compatible\n\n")
 
     -- Get redstone component
     redstone = component.redstone
 
-    -- Set TRUE on front side immediately
-    ReactorControl(REACTOR_OFF)
-
-    -- Set STATUS_ALERT_LOW to Alert, it's already stored in S-R Latch or Data Cell
+    -- Set STATUS_ALERT_LOW, it's already stored in S-R Latch or Data Cell
     SetAlertStatus(STATUS_ALERT_LOW)
 
     --------- Draw static elements ---------
-    io.write("----------------- SYSTEM STATUS ------------------\n")
+    io.write("----------------- SYSTEM STATUS -------------1.0.0\n")
     io.write("Required redstone level                     "..tostring(requiredRedstoneLevel).."\n")
     io.write("Electromagnets comparator input             "..sides[redstoneComparatorSide].."\n")
     io.write("Alert signal output                         "..sides[redstoneAlertSide].."\n")
@@ -200,17 +254,31 @@ local function main()
     io.write("Current redstone level\n")
     ----------------------------------------
 
+    -- Init vars before entering loop
+    local pendingCoreDismount = false
+
+    -- While true do - optimizaciyu v pizdu
     while (true) do
         local isConditionsOk = true
-        local pendingCoreDismount = false
+
+        UpdateScreenTime()
+        LogLine("New iteration\n")
 
         -- Internal power
         -------
         if (CheckInternalPower()) then
             UpdateScreenStatus(UI_CTRL_POWER, STR_GOOD)
+            LogLine("Power is GOOD\n")
         else
             UpdateScreenStatus(UI_CTRL_POWER, STR_BAD)
             isConditionsOk = false
+
+            if (GetInternalPower() < 0.1) then
+                pendingCoreDismount = true
+                LogLine("Power is BAD and below 10%\n")
+            else
+                LogLine("Power is BAD\n")
+            end
         end
         -------
 
@@ -218,9 +286,11 @@ local function main()
         -------
         if (CheckToolValidity()) then
             UpdateScreenStatus(UI_CTRL_TOOL, STR_GOOD)
+            LogLine("Tool is GOOD\n")
         else
             UpdateScreenStatus(UI_CTRL_TOOL, STR_BAD)
             isConditionsOk = false
+            LogLine("Tool is BAD\n")
         end
         -------
 
@@ -228,9 +298,11 @@ local function main()
         -------
         if (CheckInventorySpace()) then
             UpdateScreenStatus(UI_CTRL_SPACE, STR_GOOD)
+            LogLine("Inventory is GOOD\n")
         else
             UpdateScreenStatus(UI_CTRL_SPACE, STR_BAD)
             isConditionsOk = false
+            LogLine("Inventory is BAD\n")
         end
         -------
 
@@ -241,11 +313,13 @@ local function main()
         -------
         if (CheckElectromagnetsPower()) then
             UpdateScreenStatus(UI_CTRL_ELECTROMAGNETS, STR_GOOD)
+            LogLine("Electromagnets are GOOD\n")
         else
             UpdateScreenStatus(UI_CTRL_ELECTROMAGNETS, STR_BAD)
             isConditionsOk = false
             pendingCoreDismount = true
             SetAlertStatus(STATUS_ALERT_HIGH)
+            LogLine("Electromagnets are BAD\n")
         end
         -------
 
@@ -253,48 +327,64 @@ local function main()
         -------
         if (CheckAlertStatus()) then
             UpdateScreenStatus(UI_CTRL_ALERT, STR_GOOD)
+            LogLine("Alert is GOOD\n")
         else
             UpdateScreenStatus(UI_CTRL_ALERT, STR_BAD)
             isConditionsOk = false
+            pendingCoreDismount = true
+            LogLine("Alert is BAD\n")
 
             -- TODO: What should we do here, probably nothing?
         end
         -------
 
         if (isConditionsOk) then
-            ReactorControl(REACTOR_ON)
+            -- Moved out
+            --ReactorControl(REACTOR_ON)
+
             UpdateScreenStatus(UI_CTRL_CURRENTSTATUS, STR_READY)
             UpdateScreenStatus(UI_CTRL_BOTTOMBAR, STR_NOTHING)
+            LogLine("Conditions are GOOD\n")
         else
-            UpdateScreenStatus(UI_CTRL_CURRENTSTATUS, STR_FAIL)
+            UpdateScreenStatus(UI_CTRL_BOTTOMBAR, STR_CHECK_ASAP)
+            LogLine("Conditions are BAD\n")
 
-            if (isRunning) then
-                UpdateScreenStatus(UI_CTRL_BOTTOMBAR, STR_CHECK_ASAP)
-                note.play("E5", 2)
+            if (pendingCoreDismount) then
+                UpdateScreenStatus(UI_CTRL_CURRENTSTATUS, STR_ALERT)
+                note.play("E5", 2) -- TODO: change tone for alert
+                LogLine("if (pendingCoreDismount) pass\n")
+                ReactorControl(REACTOR_OFF)
 
-                if (pendingCoreDismount) then
-                    ReactorControl(REACTOR_OFF)
+                UpdateScreenStatus(UI_CTRL_BOTTOMBAR, STR_UNMOUNTING_CORE)
+                LogLine("Unmounting core...\n")
 
-                    UpdateScreenStatus(UI_CTRL_BOTTOMBAR, STR_UNMOUNTING_CORE)
-
-                    if (CoreUnmount()) then
-                        UpdateScreenStatus(UI_CTRL_BOTTOMBAR, STR_UNMOUNT_SUCCESS)
-                    else
-                        UpdateScreenStatus(UI_CTRL_BOTTOMBAR, STR_UNMOUNT_FAIL)
-                    end
-
-                    -- TODO: infinite beep loop?
-
-                    io.write("\n")
-                    os.exit()
+                if (CoreUnmount()) then
+                    UpdateScreenStatus(UI_CTRL_BOTTOMBAR, STR_UNMOUNT_SUCCESS)
+                    LogLine("Unmount success\n")
+                else
+                    UpdateScreenStatus(UI_CTRL_BOTTOMBAR, STR_UNMOUNT_FAIL)
+                    LogLine("Unmount failed\n")
                 end
-            else
-                UpdateScreenStatus(UI_CTRL_BOTTOMBAR, STR_CONDITIONS_NOT_MET)
+
+                -- TODO: infinite beep loop?
+                UpdateScreenTime()
+                LogLine("Exit\n")
                 io.write("\n")
                 os.exit()
+                return 1
+            else
+                UpdateScreenStatus(UI_CTRL_CURRENTSTATUS, STR_FAIL)
+                note.play("E5", 2) -- TODO: change tone for warning
             end
         end
 
+        -- If bad conditions are good or not critical, we are here
+        -- Pay attention for this
+        ReactorControl(REACTOR_ON)
+
+        --computer.pullSignal()
+        --event.pull(1)
+        LogLine("Wait for sleep()\n\n")
         os.sleep(3)
     end
 
